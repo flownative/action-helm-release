2f95e8e633e5b5aff8ccecbd2444d61ac0128605
Robert Lemke <robert@flownative.com>
2020-01-16 16:36:06 +0100

    Don't use shorthand flags for helm push call

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,4 +27,4 @@ cd "${INPUT_CHARTS_FOLDER}/${INPUT_CHART_NAME}"
 
 helm inspect chart .
 helm package .
-helm push "${INPUT_CHART_NAME}"-* "${INPUT_REPOSITORY_URL}" -u "${INPUT_REPOSITORY_USER}" -p "${INPUT_REPOSITORY_PASSWORD}" -f
+helm push "${INPUT_CHART_NAME}"-* "${INPUT_REPOSITORY_URL}" --username "${INPUT_REPOSITORY_USER}" --password "${INPUT_REPOSITORY_PASSWORD}" --force
